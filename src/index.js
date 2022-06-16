b90e5988e3a8c4fcd645d1996a12b19e41eecd0a
Alejandro Andrade <meuxdev@gmail.com>
2022-06-15 20:47:48 -0500

    Adding babel config for JS. - Project is ready for development.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("React avanazado en platzi! 🚀");
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+
+const Main = () => {
+  return (
+    <>
+      <h1>React Avanzado en Platzi 🚀</h1>
+    </>
+  );
+};
+
+root.render(<Main />);
